4a4abae9bd65c1ca408097454745b010d17493b9
Nicholas.Pleschko <nicholas.pleschko@cubx-software.com>
2015-10-22 10:57:13 +0200

    BORM-9921 Using the shared web-serve package
    +review BR-8715

--- gulpfile.js
+++ gulpfile.js
@@ -2,22 +2,9 @@ var gulp = require('gulp');
 var gutil = require('gulp-util');
 
 // used to serve the example
-var connect = require('connect');
-var static = require('serve-static');
-var open = require('open');
-
-// Serve dir at port and open the browser at path (relative to dir)
-var serveAndOpen = function(dir, port, path) {
-    connect()
-        .use(static(dir))
-        .listen(port, function() {
-            gutil.log('Starting webserver on port ' + port);
-            var openPath = 'http://localhost:' + port + '/' + path;
-            gutil.log('Opening browser at ' + openPath);
-            open(openPath);
-        });
-};
+var webserve = require('web-serve');
 
 gulp.task('serve-example', function(){
-    serveAndOpen('./', 8000, 'index.html');
+    gutil.log('Starting webserver at http://localhost:8000/index.html');
+    webserve();
 });
